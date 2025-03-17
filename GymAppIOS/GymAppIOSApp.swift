54fd2aa2322573aa737a85189610c51e75ac4792
Marcelo Ramos <marcelorcramos@gmail.com>
2025-03-17 21:09:54 +0000

    Initial Commit

---
+++ GymAppIOS/GymAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppIOSApp.swift
+//  GymAppIOS
+//
+//  Created by Marcelo Ramos on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GymAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
